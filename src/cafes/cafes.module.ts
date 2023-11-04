6c9433c681d16d09111d350a8e7c0cbe6c33364d
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2023-11-04 16:39:07 -0500

    clase cafe y cafes module

---
+++ src/cafes/cafes.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CafesComponent } from './cafes.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [CafesComponent],
+})
+export class CafesModule {}
---
+++ src/models/Cafe.ts
@@ -0,0 +1,39 @@
+export class Cafe {
+  id: string;
+  nombre: string;
+  tipo: string;
+  region: string;
+  sabor: string;
+  altura: number;
+  imagen: string;
+
+  constructor(
+    id: string,
+    nombre: string,
+    tipo: string,
+    region: string,
+    sabor: string,
+    altura: number,
+    imagen: string
+  ) {
+    this.id = id;
+    this.nombre = nombre;
+    this.tipo = tipo;
+    this.region = region;
+    this.sabor = sabor;
+    this.altura = altura;
+    this.imagen = imagen;
+  }
+
+  public static fromJson(cafeJson: any): Cafe {
+    return new Cafe(
+      cafeJson.id,
+      cafeJson.nombre,
+      cafeJson.tipo,
+      cafeJson.region,
+      cafeJson.sabor,
+      cafeJson.altura,
+      cafeJson.imagen
+    );
+  }
+}
